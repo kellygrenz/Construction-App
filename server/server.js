ecfb360ef29c0a689836fad446091cfab22736aa
adamstruppler <31010598+adamstruppler@users.noreply.github.com>
2018-02-06 19:18:19 -0700

    project
    project

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 const developerRoutes = require('./routes/developers')
+const projectRoutes = require('./routes/project')
 
 const port = 3001
 app.set('trust proxy', '127.0.0.1')
@@ -13,6 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./config/error-handler'))
 app.use('/api/developers', developerRoutes)
+app.use('/api/projects', projectRoutes)
 
 const server = app.listen(port, () => console.log(`Running on port: ${port}`))
 
