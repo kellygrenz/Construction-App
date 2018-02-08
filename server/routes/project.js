4aa27dd75ef079f206c602826b71d12b1ed0aa7f
adamstruppler <31010598+adamstruppler@users.noreply.github.com>
2018-02-07 20:27:28 -0700

    routes
    routes are done

--- server/routes/project.js
+++ server/routes/project.js
@@ -2,13 +2,13 @@ const express = require('express')
 const Router = express.Router()
 const Project = require('../models/Project')
 
-Router.route('/api/posts')
-  .post((req, res) => {
+Router.route('/')
+  .get((req, res) => {
     Project.find((err, projects) => {
       if (err) {
-        res.json({ error: err})
+        res.json({error: err})
       } else {
-        res.json({ msg: 'SUCCESS', data: projects})
+        res.json({msg: 'SUCCESS', data: projects})
       }
     })
   })
@@ -18,9 +18,20 @@ Router.route('/api/posts')
     project.setDate()
     project.save((err, savedProject) => {
       if (err) {
-        res.json({ error: err})
+        res.json({error: err})
       } else {
-        res.json({ msg: 'SUCCESS', data: savedProject})
+        res.json({msg: 'SUCCESS', data: savedProject})
+      }
+    })
+  })
+Router.route('/:projectsId')
+  .delete((req, res) => {
+    const projectId = req.params.projectId
+    Project.remove({_id: projectId}, (err, project) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: `Success on deleting ${projectId}`, data: project})
       }
     })
   })
